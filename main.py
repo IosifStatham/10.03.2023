ffbf9fe4b09e97dfa964b14cebdb458fdf8d2dca
Vlad Gorbachev <vlad-gorbachev-96@mail.ru>
2023-03-10 23:53:23 +0900

    version 2

--- main.py
+++ main.py
@@ -13,3 +13,52 @@ with open ('list of dishes\list_dish.txt', 'rt', encoding= 'utf-8' ) as file:
                })
        file.readline()
        cook_book[cook_book1] = dish
+
+
+def UPPER (dishes):
+    dishes_upper = []
+    for i in dishes:
+       i_str = str(i)
+       i_str1 = str.capitalize(i_str)
+       dishes_upper.append(i_str1)
+    return dishes_upper
+       
+def get_shop_list_by_dishes(dishes, person_count):
+    dishes1 = UPPER(dishes)
+    dishes_set = set(dishes1)
+    all_keys = cook_book.keys()
+    all_keys_set = set(all_keys)
+    res = list((dishes_set) & (all_keys_set))
+    print('Вам потребуется:')
+    for d in res:
+        dis1 = cook_book.get(d)
+        dis1_list = list(dis1)
+        for ing in dis1_list:
+            val = ing.values()
+            val_list = list(val)
+            a, b, c = val_list
+            print (f'{a} {int(b)*int(person_count)} {c}')
+  
+  
+  
+input_dishes = []  
+input_count = []    
+
+def main (dishe, coun):
+     
+    while True:
+        command = input ('Введмите команду i или Ш для подсчета необходимых продуктов для праздничного меню')
+        if command == "i" or command == "ш" :
+            input_dishes1 = input('Введите блюда которые бы Вы хотели приготовить в формате : "Блюдо1, блюдо2, и тд.')
+            input_dishes2 = input_dishes1.split(', ')
+            input_count = input('Введите колличество персон')
+            #UPPER (input_dishes2)
+            get_shop_list_by_dishes (input_dishes2, input_count)
+        #.title()
+        
+        elif command == "q":
+           print("Выход")
+           return
+            
+
+print(main(input_dishes, input_count))
